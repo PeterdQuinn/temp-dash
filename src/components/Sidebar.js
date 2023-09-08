5cf287c4d566448678e3c405070960c898bc13b0
Peter Quinn <peterdquinnsr@gmail.com>
2023-09-08 14:53:31 -0700

    updated

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,16 +1,12 @@
 export default function Sidebar() {
   return (
-    <div className="bg-gray-800 text-white w-64 space-y-6 py-7 px-2 absolute inset-y-0 left-0">
-      <a href="#" className="text-white px-2 py-1 block">
-        Home
-      </a>
-      <a href="#" className="text-white px-2 py-1 block">
-        Profile
-      </a>
-      <a href="#" className="text-white px-2 py-1 block">
-        Settings
-      </a>
-      {/* Add more links here */}
+    <div className="bg-indigo-800 text-white w-64 h-screen fixed top-0 left-0 p-6">
+      <ul className="space-y-4">
+        <li className="text-indigo-300">Dashboard</li>
+        <li>Users</li>
+        <li>Settings</li>
+        <li>Reports</li>
+      </ul>
     </div>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
       <Navbar />
       <div className="flex flex-1">
         <Sidebar />
-        <main className="flex-1 bg-gray-100 p-6">
+        <main className="flex-1 bg-gray-100 p-6 ml-64">
           <h1 className="text-3xl font-semibold text-gray-900">Welcome, Admin!</h1>
           <div className="mt-4 bg-white p-6 rounded-lg shadow-lg">
             <h2 className="text-2xl font-semibold text-gray-800">Overview</h2>
